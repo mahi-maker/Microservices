69a387612d5392f85e636e52fe4ea0bb0b404ca6
mahi-maker <mahendralekkala116@gmail.com>
2024-10-29 12:31:10 +0530

    Initial commit

---
+++ 04_Welcome_Api/src/main/java/in/dextris/controller/WelcomeRestController.java
@@ -0,0 +1,20 @@
+package in.dextris.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	@Autowired
+	private Environment env;
+	
+	@GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		String port = env.getProperty("server.port");
+		String msg="welcome to Dextris.!!! ("+ port + ")";
+		return msg;
+	}
+
+}
